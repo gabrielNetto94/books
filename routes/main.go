23f4846c945a77d7f3e77aaa9859bea699326ae9
gabrielNetto94 <silveiranetto94@gmail.com>
2024-06-18 14:30:55 -0300

    remove air.toml altera http-response

--- routes/main.go
+++ routes/main.go
@@ -1,7 +1,7 @@
 package routes
 
 import (
-	httpresponse "books/internal/http-response"
+	httpresponse "books/internal/infra/web/http-response"
 
 	"github.com/gin-gonic/gin"
 )
@@ -13,14 +13,6 @@ func InitRoutes() *gin.Engine {
 
 	router.GET("/", func(ctx *gin.Context) {
 		httpresponse.HandleResponse(ctx.Writer, 200, map[string]string{"message": "hello world"})
-	})
-
-	router.GET("/error", func(ctx *gin.Context) {
-		httpresponse.HandleError(ctx.Writer, 500, "Error message", nil)
-	})
-
-	router.GET("/no-content", func(ctx *gin.Context) {
-		httpresponse.HandleResponseNoContent(ctx.Writer)
 	})
 
 	return router
