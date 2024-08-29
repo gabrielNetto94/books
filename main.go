0c25f17354469e2c2e79ee79fd80c1dbd81a6211
gabrielNetto94 <gabriel@moinhosul.com.br>
2024-08-29 10:17:58 -0300

    test CI cache

--- main.go
+++ main.go
@@ -7,8 +7,6 @@ import (
 
 func main() {
 
-	// https://github.dev/sergicanet9/go-hexagonal-api
-
 	if err := routes.InitRoutes().Run(":3000"); err != nil {
 		log.Fatal("Error init server", err.Error())
 	}
